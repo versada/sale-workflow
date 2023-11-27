49e086cbcfb06b51141f9550f649cc8eee8113dc
Aurelija Vitkauskienė <aurelija.vitkauskiene@versada.eu>
2023-11-27 14:37:58 +0200

    [FIX] sale_advance_payment: post_init tag
    [FIX] sale_automatic_workflow_payment_mode: post_init tag
    
    https://hive.versada.eu/work_packages/22080/activity

--- sale_advance_payment/tests/test_sale_advance_payment.py
+++ sale_advance_payment/tests/test_sale_advance_payment.py
@@ -4,9 +4,10 @@
 
 from odoo import fields
 from odoo.exceptions import ValidationError
-from odoo.tests import common
+from odoo.tests import common, tagged
 
 
+@tagged("post_install", "-at_install")
 class TestSaleAdvancePayment(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
